fcd18808c5c251e4a8adb995d3c51ed90abd941e
Fərid <feridhelios@gmail.com>
2022-05-06 11:05:47 +0400

    vue project

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1><span> Vue </span> Movies Header</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <router-link to="/">
+      <h1><span> Vue </span> Movies Footer</h1>
+    </router-link>
+  </footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+  &::selection {
+    background: transparentize($color: #42b883, $amount: 0.5);
+  }
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+main {
+  min-height: 90vh;
+}
+body {
+  background: #35495e;
+}
+a {
+  text-decoration: none;
+}
+header,
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.2);
+  h1 {
+    color: #fff;
+    font-size: 28px;
+    span {
+      color: #42b883;
     }
   }
 }
---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="movie-detail">
+    <h2>Movie title {{ movie.Title }}</h2>
+    <p>
+      {{ movie.Year }}
+    </p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+    <p>
+      {{ movie.Plot }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "../env";
+export default {
+  name: "MovieDetail",
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          movie.value = data;
+        })
+        .catch((err) => console.log(err));
+    });
+    return {
+      movie,
+      route,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.movie-detail {
+  padding: 16px;
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+  p {
+    color: #fff;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
+</style>
